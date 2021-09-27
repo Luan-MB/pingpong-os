e3da3fe245b729b006b236c4626c09cbc8ed3274
Luan-MB <luanmb20@gmail.com>
2021-09-27 10:59:18 -0300

    P0:DONE

--- P0/queue.c
+++ P0/queue.c
@@ -3,6 +3,9 @@
 #include "queue.h"
 #include <stdio.h>
 
+//------------------------------------------------------------------------------
+// Conta o numero de elementos na fila
+// Retorno: numero de elementos na fila
 int queue_size (queue_t *queue) {
 
     if (!queue) // Se a fila estiver vazia
@@ -17,6 +20,12 @@ int queue_size (queue_t *queue) {
     return size;
 }
 
+//------------------------------------------------------------------------------
+// Percorre a fila e imprime na tela seu conteúdo. A impressão de cada
+// elemento é feita por uma função externa, definida pelo programa que
+// usa a biblioteca. Essa função deve ter o seguinte protótipo:
+//
+// void print_elem (void *ptr) ; // ptr aponta para o elemento a imprimir
 void queue_print (char *name, queue_t *queue, void print_elem (void*) ) {
 
     printf("%s: [",name);
@@ -36,6 +45,14 @@ void queue_print (char *name, queue_t *queue, void print_elem (void*) ) {
 
     printf("]\n");
 }
+
+//------------------------------------------------------------------------------
+// Insere um elemento no final da fila.
+// Condicoes a verificar, gerando msgs de erro:
+// - a fila deve existir
+// - o elemento deve existir
+// - o elemento nao deve estar em outra fila
+// Retorno: 0 se sucesso, <0 se ocorreu algum erro
 
 int queue_append (queue_t **queue, queue_t *elem) {
 
@@ -73,6 +90,14 @@ int queue_append (queue_t **queue, queue_t *elem) {
     return 0;
 }
 
+//------------------------------------------------------------------------------
+// Remove o elemento indicado da fila, sem o destruir.
+// Condicoes a verificar, gerando msgs de erro:
+// - a fila deve existir
+// - a fila nao deve estar vazia
+// - o elemento deve existir
+// - o elemento deve pertencer a fila indicada
+// Retorno: 0 se sucesso, <0 se ocorreu algum erro
 int queue_remove (queue_t **queue, queue_t *elem) {
 
     if (!queue) {
@@ -90,7 +115,7 @@ int queue_remove (queue_t **queue, queue_t *elem) {
         return 1;
     }
     
-    if (!(elem->prev) || !(elem->next)) {
+    if (!(elem->prev) && !(elem->next)) {
         perror("### O elemento nao pertence a nenhuma fila ###");
         return 1;
     }
