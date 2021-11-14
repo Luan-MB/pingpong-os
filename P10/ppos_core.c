d7307991d32396f277e979ae4dfe99907c7d1d3c
Luan-MB <luanmb20@gmail.com>
2021-11-14 15:15:33 -0200

    P10: Create ppos_ipc.c

--- P10/ppos_core.c
+++ P10/ppos_core.c
@@ -18,7 +18,6 @@ task_t *currentTask, *prevTask, *taskQueue, *sleepingQueue;
 task_t mainTask, dispatcherTask;
 int g_taskId = 0, g_userTasks = 0, g_taskTime;
 unsigned int g_clock = 0, g_taskActivTime = 0;
-int g_lock = 0;
 
 // estrutura que define um tratador de sinal (deve ser global ou static)
 struct sigaction action ;
@@ -348,102 +347,4 @@ static void tratador () {
 unsigned int systime () {
 
     return g_clock;
-}
-
-static void enter_cs (int *lock)
-{
-    // atomic OR (Intel macro for GCC)
-    while (__sync_fetch_and_or (lock, 1)) ;   // busy waiting
-}
- 
-static void leave_cs (int *lock)
-{
-    (*lock) = 0 ;
-}
-
-// Cria um semáforo com valor inicial "value"
-int sem_create (semaphore_t *s, int value) {
-    
-    if (!s)
-        return -1;
-    s->value = value;
-    s->sQueue = NULL;
-    s->semLock = 0;
-
-    #ifdef DEBUG
-        printf("sem_create: semaforo com valor %d criado\n",value);
-    #endif
-
-    return 0;
-}
-
-// Requisita o semáforo
-int sem_down (semaphore_t *s) {
-
-    if (!s)
-        return -1;
-
-    #ifdef DEBUG
-        printf("sem_down: tarefa %d solicitou sem_down\n",currentTask->id);
-    #endif
-    
-    enter_cs(&s->semLock);
-    s->value--;
-    leave_cs(&s->semLock);
-
-    if (s->value < 0) {
-        queue_remove((queue_t **) &taskQueue, (queue_t *) currentTask);
-        queue_append(&s->sQueue, (queue_t *) currentTask);
-        task_yield();
-    }
-    
-    return 0;
-}
-
-// Libera o semáforo
-int sem_up (semaphore_t *s) {
-
-    if (!s)
-        return -1;
-    #ifdef DEBUG
-        printf("sem_up: tarefa %d solicitou sem_up\n",currentTask->id);
-    #endif
-
-    enter_cs(&s->semLock);
-    s->value++;
-    
-    
-    if (s->sQueue) {
-
-        queue_t *firstElem = s->sQueue;
-
-        queue_remove(&s->sQueue, firstElem);
-        queue_append((queue_t **) &taskQueue, firstElem);
-    }
-    leave_cs(&s->semLock);
-    return 0;
-}
-
-// Destroi o semáforo, liberando as tarefas bloqueadas
-int sem_destroy (semaphore_t *s) {
-
-    if (!s)
-        return -1;
-    
-    int semQueueSize;
-
-    if ((semQueueSize = queue_size(s->sQueue)) > 0) {
-
-        queue_t *aux = s->sQueue;
-
-        for (int i=0; i<semQueueSize; ++i) {
-            queue_remove(&s->sQueue, aux);
-            queue_append((queue_t **) &taskQueue, aux);
-        }
-    }
-
-    #ifdef DEBUG
-        printf("sem_destroy: semaforo destruido\n");
-    #endif
-    return 0;
 }
---
+++ P10/ppos_global_vars.h
@@ -0,0 +1,9 @@
+// GRR20190363 Luan Machado Bernardt
+
+#ifndef __PPOS_GLOBAL_VARS__
+#define __PPOS_GLOBAL_VARS__
+
+extern task_t *taskQueue;
+extern task_t *currentTask;
+
+#endif
